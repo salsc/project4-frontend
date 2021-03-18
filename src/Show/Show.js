316a009d556ea3a0f6670187fa61c1df6ce8ee24
salsberyscottj <salsberyscottj@johndeere.com>
2021-03-18 09:22:58 -0500

    working well

--- src/Show/Show.js
+++ src/Show/Show.js
@@ -6,6 +6,7 @@ export default class Show extends Component {
         console.log('show', this.props)
         const recipeDetail = this.props.recipes.find((recipe) => {
             return recipe.id == this.props.match.params.id;
+
         });
         console.log('recipedetail', recipeDetail)
 
@@ -15,19 +16,19 @@ export default class Show extends Component {
                 <h2>{recipeDetail.recName}</h2>
                 <h4>{recipeDetail.recNameMore}</h4>
                 <h5>Ingredients</h5>
-                <ul>
-                    <li>{recipeDetail.strIngredient1} {recipeDetail.strMeasure1}</li>
-                    <li>{recipeDetail.strIngredient2} {recipeDetail.strMeasure2}</li>
-                    <li>{recipeDetail.strIngredient3} {recipeDetail.strMeasure3}</li>
-                    <li>{recipeDetail.strIngredient4} {recipeDetail.strMeasure4}</li>
-                    <li>{recipeDetail.strIngredient5} {recipeDetail.strMeasure5}</li>
+                <ul className="ingredList">
+                    {recipeDetail.strIngredient1 && <li>{recipeDetail.strIngredient1} {recipeDetail.strMeasure1}</li>}
+                    {recipeDetail.strIngredient2 && <li>{recipeDetail.strIngredient2} {recipeDetail.strMeasure2}</li>}
+                    {recipeDetail.strIngredient3 && <li>{recipeDetail.strIngredient3} {recipeDetail.strMeasure3}</li>}
+                    {recipeDetail.strIngredient4 && <li>{recipeDetail.strIngredient4} {recipeDetail.strMeasure4}</li>}
+                    {recipeDetail.strIngredient5 && <li>{recipeDetail.strIngredient5} {recipeDetail.strMeasure5}</li>}
                 </ul>
                 <h5>Directions</h5>
-                <ol>
-                    <li>{recipeDetail.txtDirections1}</li>
-                    <li>{recipeDetail.txtDirections2}</li>
-                    <li>{recipeDetail.txtDirections3}</li>
-                    <li>{recipeDetail.txtDirections4}</li>
+                <ol className="directList">
+                    {recipeDetail.txtDirections1 && <li>{recipeDetail.txtDirections1}</li>}
+                    {recipeDetail.txtDirections2 && <li>{recipeDetail.txtDirections2}</li>}
+                    {recipeDetail.txtDirections3 && <li>{recipeDetail.txtDirections3}</li>}
+                    {recipeDetail.txtDirections4 && <li>{recipeDetail.txtDirections4}</li>}
                 </ol>
                 <img src={recipeDetail.strRecipeImage}></img>
             </div>
